c957c0c7149b3a99202832762c94bbd24b4e4356
garvitakatyal <garvitakatyal@hotmail.com>
2021-10-27 21:04:55 -0400

    class exit

--- quizapp/ViewController.swift
+++ quizapp/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //outlets form variables and actions form a function. In this case buttons are outlets and functions both.
+    
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var questions: UILabel!
+    
+    var questionBank = [["5+2 = 9", "False"],
+                        ["9+1 = 10", "True"],
+                        ["10-3 = 6", "False"],
+                        ["8+8 = 16", "True"]]
+   var questionNumber = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateUI()
+        
+}
+
+    func updateUI(){
+        questions.text = questionBank[0][0]
+        
     }
-
-
+    
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        userInput = sender.currentTitle!
+        
+        if userInput == AnswerBank {
+            sender.backgroundColor = green
+        }
+    }
+    
+    
+    
+    
 }
 
